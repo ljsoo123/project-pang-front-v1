4bda780ae2bad34111adad6a523ae4edb215fd65
ljsoo123 <ljsoo123456@naver.com>
2020-10-07 23:36:28 +0900

    헤더 항상 렌더링

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header/Header';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 const App = () => {
   return(
-    <div>
-      <Header/>
-    </div>
+    <BrowserRouter>
+    <Header/>
+      <Switch>
+        <Route path="/"  />
+      </Switch>
+    </BrowserRouter>
+
   )
 }
 
