9b288c0108ae36442cffe52adc942b46aa757bf5
ljsoo123 <ljsoo123456@naver.com>
2020-10-13 19:13:37 +0900

    [MODIFY] 404

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Header from "./components/Header/Header";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Experience from "./components/Experience/Experience";
 
@@ -12,11 +12,36 @@ const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Route path="/" />
-      <Route path="/login" component={Login} />
-      <Route path="/experience" component={Experience} />
+      <Switch>
+        <Route exact path="/" />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/experience" component={Experience} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 };
 
+const NotFound = () => {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        width: "100vw",
+        height: "100vh",
+        background: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        top: "0",
+        left: "0",
+        color: "#2D3036",
+        fontSize: "50px",
+        fontWeight: "bolder",
+      }}
+    >
+      404 NOT FOUND
+    </div>
+  );
+};
 export default App;
