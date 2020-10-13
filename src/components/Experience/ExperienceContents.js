32301060c0cee291f69fecacf61d85973f02210c
ljsoo123 <ljsoo123456@naver.com>
2020-10-13 23:05:41 +0900

    [MODIFY] prettier

--- src/components/Experience/ExperienceContents.js
+++ src/components/Experience/ExperienceContents.js
@@ -1,17 +1,15 @@
-import React from 'react'
-import * as S from './styles'
-import ExperienceItem from './ExperienceItem'
+import React from "react";
+import * as S from "./styles";
+import ExperienceItem from "./ExperienceItem";
 
-const ExperienceContents = ({user}) => {
-    return(
-    <> 
-        <S.ExperienceMainDiv>
-            {user.map(users => (
-                <ExperienceItem users={users}></ExperienceItem>
-            ))}
-        </S.ExperienceMainDiv>
-           </>
-    )
-}
+const ExperienceContents = ({ user }) => {
+  return (
+    <>
+      {user.map((users) => (
+        <ExperienceItem users={users}></ExperienceItem>
+      ))}
+    </>
+  );
+};
 
 export default ExperienceContents;
