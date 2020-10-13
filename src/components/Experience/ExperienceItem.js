4c581ddd56a3c802e8e69ea4d304a703f1a54b01
ljsoo123 <ljsoo123456@naver.com>
2020-10-13 19:31:10 +0900

    [MODIFY] prittier

--- src/components/Experience/ExperienceItem.js
+++ src/components/Experience/ExperienceItem.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import * as S from './styles'
-import farmer from './farmer.jpg'
-const ExperienceItem = ({users}) => {
-    console.log(users)
-    const {exname, exwhere, price, userid, endtime} = users;
-    return(
-        <S.ExperienceItemMain>
-            <img src={farmer}></img>
-            <div id="title">{users.exname}</div>
-            <div>{users.exwhere}</div>
-            <div>{users.price}</div>
-            <div>{users.userid}</div>
-            <div>{users.endtime}</div>
-        </S.ExperienceItemMain>
-    )
-}
+import React from "react";
+import * as S from "./styles";
+import farmer from "./farmer.jpg";
+const ExperienceItem = ({ users }) => {
+  console.log(users);
+  const { exname, exwhere, price, endtime } = users;
+  return (
+    <S.ExperienceItemMain>
+      <img src={farmer}></img>
+      <div id="title">{users.exname}</div>
+      <S.InnerExperience>
+        <div>
+          <div id="price">{users.price}원</div>
+          <div id="exwhere">{users.exwhere}</div>
+        </div>
+        <div id="endtime">{users.endtime}</div>
+      </S.InnerExperience>
+    </S.ExperienceItemMain>
+  );
+};
 
 export default ExperienceItem;
