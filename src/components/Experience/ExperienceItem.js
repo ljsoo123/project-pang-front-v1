0b254035ce0727a18c1657d899c9668984a0a9da
ljsoo123 <ljsoo123456@naver.com>
2020-10-13 23:05:22 +0900

    [MODIFY] 필요없는 부분 삭제

--- src/components/Experience/ExperienceItem.js
+++ src/components/Experience/ExperienceItem.js
@@ -2,8 +2,9 @@ import React from "react";
 import * as S from "./styles";
 import farmer from "./farmer.jpg";
 const ExperienceItem = ({ users }) => {
+  const { id, exname, exwhere, price, endtime } = users;
+  console.log("1");
   console.log(users);
-  const { exname, exwhere, price, endtime } = users;
   return (
     <S.ExperienceItemMain>
       <img src={farmer}></img>
@@ -13,7 +14,7 @@ const ExperienceItem = ({ users }) => {
           <div id="price">{users.price}원</div>
           <div id="exwhere">{users.exwhere}</div>
         </div>
-        <div id="endtime">{users.endtime}</div>
+        <div id="endtime">10월 10일 ~ {users.endtime}</div>
       </S.InnerExperience>
     </S.ExperienceItemMain>
   );
