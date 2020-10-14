09ff7dffcc8455dd5480d8124e713980333d2e17
ljsoo123 <ljsoo123456@naver.com>
2020-10-14 09:25:12 +0900

    [MODIFY] 페이지 이동 추가

--- src/components/Header/MainHeader/MainHeader.js
+++ src/components/Header/MainHeader/MainHeader.js
@@ -1,24 +1,29 @@
-import React  from 'react'
-import * as S from './styles';
-import logo1 from '../../../asset/logo.png';
+import React from "react";
+import * as S from "./styles";
+import logo1 from "../../../asset/logo.png";
 
 const MainHeader = () => {
-    return (
-        <>
-            <S.GlobalStyle/>
-            <S.Maindiv>     
-                <S.Logo>
-                    <S.MainText>일섬이조</S.MainText>
-                    <img src={logo1}></img>
-                </S.Logo  >
-                <S.Text>
-                    <div>먹거리 체험</div>
-                    <div>상품구매</div>
-                </S.Text> 
-            </S.Maindiv>
-        </>
-    )
-}
-
+  const buyClick = () => {
+    window.location.href = "./buy";
+  };
+  const experienceClick = () => {
+    window.location.href = "./experience";
+  };
+  return (
+    <>
+      <S.GlobalStyle />
+      <S.Maindiv>
+        <S.Logo>
+          <S.MainText>일섬이조</S.MainText>
+          <img src={logo1}></img>
+        </S.Logo>
+        <S.Text>
+          <div onClick={experienceClick}>먹거리 체험</div>
+          <div onClick={buyClick}>상품구매</div>
+        </S.Text>
+      </S.Maindiv>
+    </>
+  );
+};
 
 export default MainHeader;
